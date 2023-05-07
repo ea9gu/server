3c217c436dd3d63b5b4f011344c687da2896b941
Youmin Kim <march02198@gmail.com>
2023-05-08 00:30:22 +0900

    db setting

--- gongcheck/gongcheck_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GongcheckAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gongcheck_app'
--- gongcheck/gongcheck_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Account(models.Model):
-    student_id=models.CharField(max_length=7)
-    password=models.CharField(max_length=12)
-    name=models.CharField(max_length=10)
