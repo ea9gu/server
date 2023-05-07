1a90d15cd03f8736742ee85d7df33a2c7019690a
yesyeseo <kingofthefall538@gmail.com>
2023-05-07 16:51:03 +0900

    manage users with username, not emails [#5]

--- gongcheck/accounts/urls.py
+++ gongcheck/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')), # 해당 라인 추가
+    path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
 ]
--- gongcheck/gongcheck/urls.py
+++ gongcheck/gongcheck/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/v1/', include('accounts.urls')),
+    # path('api/accounts/v1/', include('accounts.urls')),
+    path('api/account/', include('accounts.urls')),
 ]
