893bd93b18332aa444774a52ebd46fcc48a6c71c
Youmin Kim <march02198@gmail.com>
2023-05-08 22:09:39 +0900

    change db setting

--- gongcheck/gongcheck/settings.py
+++ gongcheck/gongcheck/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_DATABASE"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST", 'mysql-server'),
+        'HOST': 'mysql-server',
         'PORT': os.environ.get("DB_PORT", '3306'),
     }
 }
