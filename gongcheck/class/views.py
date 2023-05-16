ea7a91aaa4276ffeb9ef1292141795af8d90d481
yesyeseo <kingofthefall538@gmail.com>
2023-05-16 23:14:15 +0900

    [#16] course create api 생성 및 테스트 완료

--- gongcheck/class/views.py
+++ gongcheck/class/views.py
@@ -6,21 +6,28 @@ from django.contrib.auth.models import User
 import csv
 from io import StringIO
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def create_course(request):
     if request.method == 'POST':
-        course_name = request.POST.get('course_name')
+        data = json.loads(request.body)
+        course_name = data.get('course_name')
+        professor_id = data.get('professor_id')
+
+        if not course_name:
+            return JsonResponse({'status': 'error', 'message': '수업 이름이 제공되지 않았습니다.'})
 
         # 가장 마지막 번호로 course_id 생성
         last_course = Course.objects.last()
         if last_course:
             last_course_id = int(last_course.course_id)
             course_id = str(last_course_id + 1)
-        else: course_id = '1'
+        else:
+            course_id = '1'
 
         # Course 생성
-        course = Course(course_id=course_id, name=course_name)
+        course = Course(course_id=course_id, name=course_name, professor_id=professor_id)
         course.save()
 
         return JsonResponse({'status': 'success'})
