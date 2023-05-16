5c202dc829f0c16c95d6c3b077dcd6a2f2e1a582
yesyeseo <kingofthefall538@gmail.com>
2023-05-16 17:20:34 +0900

    [#12] get device when student_id POST req cmae

--- gongcheck/serial/urls.py
+++ gongcheck/serial/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('save-device/', views.save_device, name='save_device'),
+    path('get-device/', views.get_device, name='get_device'),
 ]
--- gongcheck/serial/views.py
+++ gongcheck/serial/views.py
@@ -45,3 +45,40 @@ def save_device(request):
         return JsonResponse({'status': 'success'})
 
     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
+
+@csrf_exempt
+def get_device(request):
+    if request.method == 'POST':
+        # 프론트에서 전달된 데이터 받기
+        data = json.loads(request.body)
+        student_id = data.get('student_id')
+
+        # Device 모델에서 같은 device_name을 가진 최근의 데이터 찾기
+        recent_device = Device.objects.filter(student_id=student_id).first()
+
+        # 디바이스 정보가 있다면 반환
+        if recent_device:
+            response_data = {
+                'status': 'success',
+                'message': '등록된 디바이스가 있습니다',
+                'device_id': recent_device.id,
+                'device_name': recent_device.device_name,
+                'timestamp': recent_device.timestamp
+            }
+            return JsonResponse(response_data)
+        
+        # Device 모델에 데이터 저장
+        device = Device.objects.create(
+            student_id=student_id,
+            device_name=device_name,
+            device_serial=device_serial
+        )
+
+        # 현재 저장된 Device 출력
+        devices = Device.objects.all().values()
+        device_list = list(devices)
+        print(device_list)
+
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
