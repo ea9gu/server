c00b6f3e10dee089e75111e0db4552544525c3b2
yesyeseo <kingofthefall538@gmail.com>
2023-05-16 16:28:56 +0900

    change device filter to student_id and response data [#12]

--- gongcheck/serial/views.py
+++ gongcheck/serial/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Device
-from datetime import datetime, timedelta
 import json
+from django.utils import timezone
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,19 +11,28 @@ def save_device(request):
     if request.method == 'POST':
         # 프론트에서 전달된 데이터 받기
         data = json.loads(request.body)
+        student_id = data.get('student_id')
         device_name = data.get('device_name')
         device_serial = data.get('device_serial')
 
         # Device 모델에서 같은 device_name을 가진 최근의 데이터 찾기
-        two_weeks_ago = datetime.now() - timedelta(weeks=2)
-        recent_device = Device.objects.filter(device_name=device_name, timestamp__gte=two_weeks_ago).first()
+        two_weeks_ago = timezone.now() - timezone.timedelta(weeks=2)
+        recent_device = Device.objects.filter(student_id=student_id, timestamp__gte=two_weeks_ago).first()
 
         # 만약 최근 2주 내에 같은 이름의 디바이스가 등록되었다면, 에러 메시지 반환
         if recent_device:
-            return JsonResponse({'status': 'error', 'message': '이미 등록된 디바이스입니다.'})
-
+            response_data = {
+                'status': 'error',
+                'message': '이미 등록된 디바이스입니다.',
+                'device_id': recent_device.id,
+                'device_name': recent_device.device_name,
+                'timestamp': recent_device.timestamp
+            }
+            return JsonResponse(response_data)
+        
         # Device 모델에 데이터 저장
         device = Device.objects.create(
+            student_id=student_id,
             device_name=device_name,
             device_serial=device_serial
         )
