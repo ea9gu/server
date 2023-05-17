79c8bda97921c3d06c14ac771869ff60443232ef
yesyeseo <kingofthefall538@gmail.com>
2023-05-17 15:40:07 +0900

    교수 csv 처리: 중복 추가 제거, 유효 학생만 저장 [#16]

--- gongcheck/class/views.py
+++ gongcheck/class/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Course, StudentCourse
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
 
 import csv
 from io import StringIO
-from django.views.decorators.csrf import csrf_exempt
 import json
+
 
 @csrf_exempt
 def create_course(request):
@@ -34,6 +36,7 @@ def create_course(request):
 
     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
 
+
 @csrf_exempt
 def enroll_students(request):
     if request.method == 'POST':
@@ -45,24 +48,34 @@ def enroll_students(request):
         
         # CSV 데이터를 파싱하여 학생 번호를 추출하고, StudentCourse에 추가
         students_added = []
+        students_not_found = []
         csv_reader = csv.reader(StringIO(csv_data))
         for row in csv_reader:
             student_id = row[0]  # 학생 번호가 있는 열을 지정해야 함
             
             # User 정보 확인
-            try: user = User.objects.get(username=student_id, flag=0)
+            try:
+                user = User.objects.get(username=student_id, flag=0)
+                
+                # StudentCourse에 추가하기 전에 중복 여부 확인
+                try:
+                    StudentCourse.objects.get(student_id=user.username, course_id=course_id)
+                    students_not_found.append(student_id)
+                except ObjectDoesNotExist:
+                    course = Course.objects.get(course_id=course_id)
+                    student_course = StudentCourse(student_id=user.username, course_id=course.course_id)
+                    student_course.save()
+                    students_added.append(student_id)
+                    
             except User.DoesNotExist:
-                return JsonResponse({'status': 'error', 'message': f'학생 번호 {student_id}에 해당하는 정보가 없거나 유효하지 않습니다.'})
-            
-            # StudentCourse에 추가
-            course = Course.objects.get(course_id=course_id)
-            student_course = StudentCourse(student_id=user.username, course_id=course.course_id)
-            student_course.save()
-            
-            students_added.append(student_id)
+                students_not_found.append(student_id)
         
-        return JsonResponse({'status': 'success', 'students_added': students_added})
+        response_data = {
+            'status': 'success',
+            'students_added': students_added,
+            'students_not_found': students_not_found
+        }
+        return JsonResponse(response_data)
     
     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
-
 
