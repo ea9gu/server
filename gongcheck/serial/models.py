c62592ddfdfc0a21fbb602d4b69659e1f2620c8e
yesyeseo <kingofthefall538@gmail.com>
2023-05-18 19:16:31 +0900

    fix to response data and update when exists not create [#12]

--- gongcheck/serial/models.py
+++ gongcheck/serial/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Device(models.Model):
     id = models.AutoField(primary_key=True)
+    student_id = models.CharField(max_length=10)
     device_name = models.CharField(max_length=100)
     device_serial = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
-    student_id = models.CharField(max_length=100)
