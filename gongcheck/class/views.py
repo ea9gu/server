ffb68cd38366bb9819be6749d649a5c8395d58a5
yesyeseo <kingofthefall538@gmail.com>
2023-05-18 14:24:29 +0900

    change course_id to course instance

--- gongcheck/class/views.py
+++ gongcheck/class/views.py
@@ -9,74 +9,8 @@ import csv
 from io import StringIO
 import json
 
-# @csrf_exempt
-# def create_course(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         course_name = data.get('course_name')
-#         professor_id = data.get('professor_id')
-
-#         if not course_name:
-#             return JsonResponse({'status': 'error', 'message': '수업 이름이 제공되지 않았습니다.'})
-
-#         # 가장 마지막 번호로 course_id 생성
-#         last_course = Course.objects.last()
-#         if last_course:
-#             last_course_id = int(last_course.course_id)
-#             course_id = str(last_course_id + 1)
-#         else:
-#             course_id = '1'
-
-#         # Course 생성
-#         course = Course(course_id=course_id, name=course_name, professor_id=professor_id)
-#         course.save()
-
-#         return JsonResponse({'status': 'success'})
-
-#     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
-
-
-# @csrf_exempt
-# def enroll_students(request):
-#     if request.method == 'POST':
-#         course_id = request.POST.get('course_id')
-#         csv_file = request.FILES['csv_file']
-
-#         # CSV 파일을 문자열로 읽어옴
-#         csv_data = csv_file.read().decode('utf-8')
-        
-#         # CSV 데이터를 파싱하여 학생 번호를 추출하고, StudentCourse에 추가
-#         students_added = []
-#         students_not_found = []
-#         csv_reader = csv.reader(StringIO(csv_data))
-#         for row in csv_reader:
-#             student_id = row[0]  # 학생 번호가 있는 열을 지정해야 함
-            
-#             # User 정보 확인
-#             try:
-#                 user = User.objects.get(username=student_id, flag=0)
-                
-#                 # StudentCourse에 추가하기 전에 중복 여부 확인
-#                 try:
-#                     StudentCourse.objects.get(student_id=user.username, course_id=course_id)
-#                     students_not_found.append(student_id)
-#                 except ObjectDoesNotExist:
-#                     course = Course.objects.get(course_id=course_id)
-#                     student_course = StudentCourse(student_id=user.username, course_id=course.course_id)
-#                     student_course.save()
-#                     students_added.append(student_id)
-                    
-#             except User.DoesNotExist:
-#                 students_not_found.append(student_id)
-        
-#         response_data = {
-#             'status': 'success',
-#             'students_added': students_added,
-#             'students_not_found': students_not_found
-#         }
-#         return JsonResponse(response_data)
-    
-#     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
+from datetime import datetime, timedelta
+from freq.models import AudioFile
 
 @csrf_exempt
 def create_and_enroll(request):
@@ -92,9 +26,13 @@ def create_and_enroll(request):
         if not course_name or not professor_id or not course_id or not csv_file:
             return JsonResponse({'status': 'error', 'message': '요청 데이터가 부족합니다.'})
 
-        # Create the course
-        course = Course(course_id=course_id, name=course_name, professor_id=professor_id)
-        course.save()
+        # 새로운 course이면 생성하고, 아니면 기존의 data를 불러오도록 함
+        course, created = Course.objects.get_or_create(course_id=course_id, defaults={'name': course_name, 'professor_id': professor_id})
+
+        if not created:
+            course.name = course_name
+            course.professor_id = professor_id
+            course.save()
 
         # CSV 파일을 문자열로 읽어옴
         csv_data = csv_file.read().decode('utf-8')
@@ -112,10 +50,10 @@ def create_and_enroll(request):
 
                 # StudentCourse에 추가하기 전에 중복 여부 확인
                 try:
-                    StudentCourse.objects.get(student_id=user.username, course_id=course_id)
+                    StudentCourse.objects.get(student_id=user.username, course_id=course)
                     students_not_found.append(student_id)
                 except ObjectDoesNotExist:
-                    student_course = StudentCourse(student_id=user.username, course_id=course.course_id)
+                    student_course = StudentCourse(student_id=user.username, course_id=course)
                     student_course.save()
                     students_added.append(student_id)
 
@@ -131,17 +69,32 @@ def create_and_enroll(request):
 
     return JsonResponse({'status': 'error', 'message': 'POST 요청이 아닙니다.'})
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import StudentCourse
-
-@api_view(['POST'])
 @csrf_exempt
 def send_signal_to_flutter(request):
-    class_id = request.data.get('class_id')
-    
-    student_ids = StudentCourse.objects.filter(course_id=class_id).values_list('student_id', flat=True)
-    
-    # TODO: 플러터 앱에 신호를 보내는 코드 추가
-    
-    return Response({'status': 'success'})
+    if request.method == 'POST':
+        class_id = request.data.get('class_id')
+        student_id = request.data.get('student_id')
+
+        # Check if both class_id and student_id are provided
+        if not class_id or not student_id:
+            return JsonResponse({'status': 'error'})
+
+        # Get the current datetime
+        current_datetime = datetime.now()
+
+        # Calculate the datetime threshold (10 minutes ago)
+        threshold_datetime = current_datetime - timedelta(minutes=10)
+
+        try:
+            latest_audio_file = AudioFile.objects.filter(class_id=class_id, student_id=student_id, created_at__gte=threshold_datetime).latest('created_at')
+
+            # Check if the latest audio file is within the 10-minute timeframe
+            if latest_audio_file.created_at >= threshold_datetime:
+                return JsonResponse({'status': 'check'})
+            else:
+                return JsonResponse({'status': 'bluecheck'})
+
+        except AudioFile.DoesNotExist:
+            return JsonResponse({'status': 'bluecheck'})
+
+    return JsonResponse({'status': 'error'})
